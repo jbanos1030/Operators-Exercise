3aea6bb37336630c538141150590b48b1c9c3565
jbanos1030 <67976657+jbanos1030@users.noreply.github.com>
2020-07-16 13:20:19 -0500

    Add files via upload

---
+++ Jessica_B_OperatorsExercise.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+//Input variables
+//Arithmetic operators: preforms the tasks that you expect them to do so
+// Addition, Subtration, Multiplication & Division are considered Arithmetic Operators
+
+
+var a = 4
+var b = 4
+
+//Defines operations
+var c = a+b
+var d = a-b
+var e = a*b
+var f = a / b
+
+//Comparison Operators
+
+1 == 1
+5 != 1
+4 > 2
+3 < 1
+2 >= 8
+6 <= 3
+
+//Prints results
+var A = 4; var B = 4; print(A,B)
+var C = 8; var D = 0; print(C,D)
+var E = 16; var F = 1; print(E,F)
+
+
+
+
